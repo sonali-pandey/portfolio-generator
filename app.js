d59975fe2ee3e32b7d8155d198adae6233ca8bd2
Sona <sonali23.1293@gmail.com>
2021-07-14 16:34:47 -0400

    inquirer

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if(err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if(err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
